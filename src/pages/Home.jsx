3db65932b9fa508a532687ef755b08b5c798cd7d
Razeb Enyi <enyirazeb@gmail.com>
2022-03-24 16:28:08 +0100

    Theme update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { OutlineButton } from '../components/button/Button';
+
 // import HeroSlide from '../components/hero-slide/HeroSlide';
 import MovieList from '../components/movie-list/MovieList';
-
+import Button from '../components/button/Button';
 import { category, movieType, tvType } from '../api/tmdbApi';
 
 import HeroSlide from '../components/hero-slide/HeroSlide';
+
 
 const Home = () => {
     return (
@@ -16,9 +17,11 @@ const Home = () => {
             <div className="container">
                 <div className="section mb-3">
                     <div className="section__header mb-2">
-                        <h2>Trending Movies</h2>
+                        <h1>Trending Movies</h1>
                         <Link to="/movie">
-                            <OutlineButton className="small">View more</OutlineButton>
+                            {/* <OutlineButton className="small">View more</OutlineButton> */}
+                            <Button>View More</Button>
+                            
                         </Link>
                     </div>
                     <MovieList category={category.movie} type={movieType.popular}/>
@@ -26,9 +29,9 @@ const Home = () => {
 
                 <div className="section mb-3">
                     <div className="section__header mb-2">
-                        <h2>Top Rated Movies</h2>
+                        <h1>Top Rated Movies</h1>
                         <Link to="/movie">
-                            <OutlineButton className="small">View more</OutlineButton>
+                          <Button>View More</Button>
                         </Link>
                     </div>
                     <MovieList category={category.movie} type={movieType.top_rated}/>
@@ -37,20 +40,20 @@ const Home = () => {
 
                 <div className="section mb-3">
                     <div className="section__header mb-2">
-                        <h2>Trending TV</h2>
+                        <h1>Trending TV</h1>
                         <Link to="/tv">
-                            <OutlineButton className="small">View more</OutlineButton>
+                          <Button>View More</Button>
                         </Link>
                     </div>
                     <MovieList category={category.tv} type={tvType.popular}/>
                 </div>
-                
+
 
                 <div className="section mb-3">
                     <div className="section__header mb-2">
-                        <h2>Top Rated TV</h2>
+                        <h1>Top Rated TV</h1>
                         <Link to="/tv">
-                            <OutlineButton className="small">View more</OutlineButton>
+                        <Button>View More</Button>
                         </Link>
                     </div>
                     <MovieList category={category.tv} type={tvType.top_rated}/>
